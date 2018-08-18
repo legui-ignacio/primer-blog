b9b4aa678cc322cd32d6e8c39556d39d49dca140
Admin <legui.ignacio@gmail.com>
2018-08-18 18:14:30 -0300

    Agrego planilla y modifico la anterior para postear desde la base de datos

--- blogg/views.py
+++ blogg/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-	return render(request, 'blogg/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blogg/post_list.html', {'posts': posts})
+
+def horario(request):
+	return render(request, 'blogg/hora.html', {})
 
