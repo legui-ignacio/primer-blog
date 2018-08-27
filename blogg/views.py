f537920e062ea128f511310df92b57cba411b8d5
Admin <legui.ignacio@gmail.com>
2018-08-27 20:57:39 -0300

    Agregue extensiones para nuevas ventanas desde el titulo del post

--- blogg/views.py
+++ blogg/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,3 +9,6 @@ def post_list(request):
 def horario(request):
 	return render(request, 'blogg/hora.html', {})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogg/post_detail.html', {'post': post})
